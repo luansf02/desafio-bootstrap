72f117a69a682f9fa0b04e3dc189a0e2dc37e2c5
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-08-22 09:18:01 -0300

    garantindo que a sidebar recolha em telas menores

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("overlay");
+
+  sidebar.classList.toggle("show");
+  overlay.classList.toggle("show");
+}
+
 function updateTime() {
   const now = new Date();
   const timeString = now.toLocaleTimeString("pt-BR");
